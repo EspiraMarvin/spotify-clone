51a6f2263c3cc5c9c45df2376e7d946dd51590f4
marvin espira <marvinespira@adalabsafrica.com>
2021-12-15 00:13:53 +0300

    feat: useState & Props for search component

--- components/Body.js
+++ components/Body.js
@@ -1,9 +1,11 @@
 import Search from "./Search"
+import { useState } from "react"
 
 function Body() {
+    const [search, setSearch] = useState("")
     return (
         <section className="ml-24 bg-black py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
-            <Search />
+            <Search search={search} setSearch={setSearch} />
         </section>
     )
 }
--- components/Search.js
+++ components/Search.js
@@ -1,7 +1,8 @@
-function Search() {
+function Search({search, setSearch}) {
     return (
         <div className="max-w-[1150px] bg-[#1a1a1a] rounded-full overflow-hidden border-2 border-[#333333] p-1.5 px-5 pr-8 flex items-center">
             <div className="flex-shrink-0 w-4 h-4 border-2 rounded-full animate-pulse"></div>
+            <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}/>
         </div>
     )
 }
