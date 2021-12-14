15e0d06494473afc9e6bca4656966fdf4d1cf25b
marvin espira <marvinespira@adalabsafrica.com>
2021-12-15 00:04:53 +0300

    feat: add tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("tailwind-scrollbar"),
+    require("tailwind-scrollbar-hide"),
+    require("@tailwindcss/forms")
+  ],
 }
