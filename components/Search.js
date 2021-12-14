e933686db4e46d695e9463d86bef2624713b2254
marvin espira <marvinespira@adalabsafrica.com>
2021-12-15 00:25:10 +0300

    feat: search input

--- components/Search.js
+++ components/Search.js
@@ -2,7 +2,11 @@ function Search({search, setSearch}) {
     return (
         <div className="max-w-[1150px] bg-[#1a1a1a] rounded-full overflow-hidden border-2 border-[#333333] p-1.5 px-5 pr-8 flex items-center">
             <div className="flex-shrink-0 w-4 h-4 border-2 rounded-full animate-pulse"></div>
-            <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}/>
+            <input type="text" 
+                value={search} onChange={(e) => setSearch(e.target.value)}
+                className="bg-[#1a1a1a] text-white border-none lg:w-full focus:ring-0 outline-none placeholder-[#fafafa] text-xs" 
+                placeholder="Search..."
+             />
         </div>
     )
 }
